f545fb3fcfcafc6c7d5d563b41130cb6ed714e98
maulikpandya33 <maulikpandya@team.bigscal.com>
2026-09-04 16:54:44 +0530

    Added Java code to reverse a number.

---
+++ ReverseNumber.java
@@ -0,0 +1,14 @@
+// Java Program to Reverse a Number
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int number = 12345;
+        int reversedNumber = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+        System.out.println("Reversed Number: " + reversedNumber);
+    }
+}
